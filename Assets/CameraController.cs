f53cc8c1a78927fefae2186f0addc2bb3a8722fc
tajima <programming.tajima@gmail.com>
2025-08-06 13:04:56 +0900

    ゴール判定

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    // プレイヤーを宣言
     GameObject player;
 
     void Start()
     {
+        // ネコをアタッチ
         this.player = GameObject.Find("cat_0");
     }
 
     void Update()
     {
+        // プレイヤーの位置を取得
         Vector3 playerPos = this.player.transform.position;
+
+        // カメラの位置をプレイヤーのY位置に合わせる
         transform.position = new Vector3(
             transform.position.x, playerPos.y, transform.position.z
         );
